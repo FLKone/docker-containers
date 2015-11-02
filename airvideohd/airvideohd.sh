fd7dd33f252089f6b928bb63639ed75a4f414df4
FLKone <FLKone@users.noreply.github.com>
2015-11-02 22:55:43 +0100

    AirVideoHD, add HEVC support

--- airvideohd/airvideohd.sh
+++ airvideohd/airvideohd.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/var/run/dbus/system_bus_socket
 exec /sbin/setuser nobody /AirVideoServerHD --config=/config/server.properties >> /var/log/airvideohd.log 2>&1
